d1aecf33356178aab561fbd0ce33fa450bbb02c5
Tanaka Naoto <gta_youtube@yahoo.co.jp>
2019-02-15 22:44:09 +0900

    For Homebrew push

---
+++ gitblamer.rb
@@ -0,0 +1,28 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Gitblamer < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/Rasukarusan/homebrew-git-blamer.git"
+  version "0.0.1"
+  sha1 "087db08ce1d70d14123d12c526cc0b1858b51fb6"
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install 'bin/git-blamer'
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-git-blamer`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
