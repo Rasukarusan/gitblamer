89022d635ab94753f7aaa7cf58cd0c4ee366b547
Rasukarusan <gta_youtube@yahoo.co.jp>
2021-01-22 18:01:52 +0900

    update action

--- .github/actions/commit-summary/src/index.ts
+++ .github/actions/commit-summary/src/index.ts
@@ -1,19 +1,21 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 
-const exec_command = async () => {
-  await exec.exec('ls');
-  await exec.exec('which git');
-  // do smt with bar
-};
+const exec_command = async (command: string) => {
+  return await exec.exec(command);
+}
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const newTag = core.getInput('ref');
   console.log(`new_tag: ${newTag} !`);
-  exec_command()
-  core.setOutput("summary", 'this is summary!!');
 
+  const preTag = exec_command('git tag --sort=-creatordate | sed -n 2p')
+  console.log(`pre_tag: ${preTag} !`);
+
+  const summary = exec_command(`git log --oneline --pretty=tformat:"%h %s" ${preTag}..${newTag}`)
+  console.log(summary);
+
+  core.setOutput("summary", 'this is summary!!');
 } catch (error) {
   core.setFailed(error.message);
 }
